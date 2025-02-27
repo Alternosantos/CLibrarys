c78805cb3f2401f40d1e39f3238cf52653daaf54
Alberto Santos <alberto1fsantos@gmail.com>
2025-02-27 15:27:13 +0000

    second commit

--- libft.h
+++ libft.h
@@ -25,7 +25,7 @@ int ft_isalnum(int c){
 	
 }
 
-int ft_isalnum(int c){
+int ft_isascci(int c){
 	if(c >= 0 && c <= 127 ){
 		return 1;
 	}else {
@@ -56,6 +56,94 @@ int ft_tolower(int c){
 		return 0;
 	}
 
+}
+
+int ft_strlen(const char *c){
+	int counter = 0;
+	while(*c != '\0'){
+		counter++;
+		c++;
+	}
+	return counter;
+
+}
+
+int ft_strchr(char *c, char foc){
+	int counter = 0;
+	while(*c != '\0'){
+		if(*c == foc){
+			return counter;
+		}
+		c++;
+		counter++;
+	}
+	return 0;
+}
+
+int ft_strrchr(char *c, char foc){
+	int counter = 0;
+	int last = 0;
+	while(*c != '\0'){
+		if(*c == foc){
+			last = counter;
+		}
+		c++;
+		counter++;
+	}
+	return last;
+}
+
+
+int ft_strcmp(char *a, char *b){
+	while(a != '\0' && b != '\0'){
+		if (a != b){
+			return 1;
+		}else{
+			return 0;
+		}
+		a++;
+		b++;
+	}
+}
+
+char *ft_strnstr(const char *c, const char *a, int len)
+{
+    int needle_len;
+    if (*a == '\0')
+        return (char *)c;
+    needle_len = ft_strlen(a);
+    while (*c != '\0' && len >= needle_len)
+    {
+        if (*c == *a)
+        {
+            int i = 0;
+            while (i < needle_len && c[i] == a[i])
+                i++;
+            
+            if (i == needle_len)
+                return (char *)c;
+        }
+        
+        c++;
+        len--;
+    }
+    
+}
+
+int ft_strlcpy(char *c,char *a, int len){
+	int comp = ft_strlen(a);
+	int counter = 0;
+	if(  comp == 0){
+		return comp;
+	}
+	while(counter < len-1){
+		*c = *a;
+		c++;
+		a++;
+		counter++;
+	}
+	*c = '\0';
+	return comp;
 }
 
 
---
+++ teste.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "libft.h"
+
+int main()
+{
+    char str[10] ;
+    //char *result = ft_strnstr("Hello world", "world", 13);
+    //printf ("\n%d" ,ft_strlen(str));
+    //printf ("\n%d\n" ,ft_strchr(str, 'o'));
+    //printf ("\n%d\n" ,ft_strrchr(str, 'o'));
+    //printf ("\n%d\n" ,ft_strcmp("Hello", "foda se"));
+    //printf ("\n%s\n" ,result);
+    //printf("%d\n",ft_strlcpy(str, "World", 10));
+
+    
+
+    
+}
